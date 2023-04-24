0a5ef04c66d52dfbb312fc8c9c0ab7b7c498674a
germanbava <130782150+germanbava@users.noreply.github.com>
2023-04-24 09:10:57 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hola german</h1>
+      <h2>inicuiando en sandBox!</h2>
+    </div>
+  );
+}
